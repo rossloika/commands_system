2d8e290c308e40929842b7d747c5d508c09136ef
Ross <rossyderp24@outlook.com>
2021-06-20 17:30:18 -0400

    Updates to admin, checking for trello and temporary ban before player joins game

---
+++ src/server/commands_system/commands/checktempban.lua
@@ -0,0 +1,53 @@
+-- FIX THIS ERROR FOR TEMP BAN!
+local commands_system = script.Parent.Parent
+local commands_system = script.Parent.Parent
+local scripts_folder = commands_system.scripts
+local settings_module = require(commands_system.settings)
+local Command = require(scripts_folder.command)
+local temporary_ban = require(scripts_folder.temporary_ban)
+local send_game_notification = require(scripts_folder.send_notification)
+local send_webhook = require(scripts_folder.send_webhook)
+
+-- Find player via a string
+local function find_player(player)
+	for _, players in ipairs(game.Players:GetPlayers()) do
+		if player:lower() == players.Name:lower():sub(1, #player:lower()) then 
+			return game.Players:FindFirstChild(tostring(players))
+		end
+	end
+end
+
+local function send_notification(admin, player, result)
+	print(player.UserId)
+    if result then
+        local notification_data = {
+            player = admin,
+            title = "Temporary Ban!",
+            text = tostring(player).." Has a Temporary Ban!",
+            icon = "rbxassetid://6537654035",
+            waitTime = 3
+        }
+        send_game_notification.send(notification_data)
+    else
+        local notification_data = {
+            player = admin,
+            title = "Temporary Ban!",
+            text = tostring(player).." Does not have Temporary Ban!",
+            icon = "rbxassetid://6537654035",
+            waitTime = 3
+        }
+        send_game_notification.send(notification_data)
+    end
+end
+
+local function check_temp_ban(admin, player, banReason)
+    return temporary_ban.find(player.UserId)
+end
+
+return Command.new("checkban", function(args)
+	if check_temp_ban(args.player, find_player(args.command_arguments[1]), args.combined_command_arguments) then
+        send_notification(args.player, find_player(args.command_arguments[1]), true)
+    else
+        send_notification(args.player, find_player(args.command_arguments[1]), false)
+    end
+end)
---
+++ src/server/commands_system/commands/unban.lua
@@ -0,0 +1,54 @@
+-- FIX THIS ERROR FOR TEMP BAN!
+local commands_system = script.Parent.Parent
+local commands_system = script.Parent.Parent
+local scripts_folder = commands_system.scripts
+local settings_module = require(commands_system.settings)
+local Command = require(scripts_folder.command)
+local temporary_ban = require(scripts_folder.temporary_ban)
+local send_game_notification = require(scripts_folder.send_notification)
+local send_webhook = require(scripts_folder.send_webhook)
+
+-- Find player via a string
+local function find_player(player)
+	for _, players in ipairs(game.Players:GetPlayers()) do
+		if player:lower() == players.Name:lower():sub(1, #player:lower()) then 
+			return game.Players:FindFirstChild(tostring(players))
+		end
+	end
+end
+
+local function send_notification(admin, player, banReason)
+	print(player.UserId)
+	local webhook_data = {
+		username = tostring(player).." Unbanned!",
+		avatarUrl = string.format("https://www.roblox.com/bust-thumbnail/image?userId=%s&width=420&height=420&format=png", player.UserId),
+		title = "**Temporary Ban System**",
+		description = tostring(player).." Has been Unbanned!",
+		color = 0x0dcbff,
+		fields = {
+			{
+				name = "Reason",
+				value = banReason,
+			},
+		}
+	}
+	local notification_data = {
+		player = admin,
+		title = "Unbanned!",
+		text = tostring(player).." Has been Unbanned!",
+		icon = "rbxassetid://6537654035",
+		waitTime = 3
+	}
+
+	send_game_notification.send(notification_data)
+	send_webhook.send("adminLogs", webhook_data)
+end
+
+local function send_unban(admin, player, banReason)
+    return temporary_ban.find(player.UserId)
+end
+
+return Command.new("unban", function(args)
+	send_unban(args.player, find_player(args.command_arguments[1]), args.combined_command_arguments)
+    send_notification(args.player, find_player(args.command_arguments[1]), args.combined_command_arguments)
+end)
