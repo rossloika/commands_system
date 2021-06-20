61432bc2ef389f6e923a8b882a687f4b843bedec
Ross <rossyderp24@outlook.com>
2021-06-20 17:29:10 -0400

    Updates to admin, checking for trello and temporary ban before player joins game

--- src/server/commands_system/scripts/temporary_ban.lua
+++ src/server/commands_system/scripts/temporary_ban.lua
@@ -1,17 +1,17 @@
 local ban = {}
 -- Temporary Ban List
-local bansList = {}
+ban.bansList = {}
 
 function ban.insert(data)
-	return table.insert(bansList, data.userid, data)
+	return table.insert(ban.bansList, data.userid, data)
 end
 
 function ban.find(userid)
-	return bansList[userid]
+	return ban.bansList[userid]
 end
 
 function ban.remove(userid)
-	bansList[userid] = nil
+	ban.bansList[userid] = nil
 	return true
 end
 
--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,2 +1,48 @@
 require(script.commands_system.scripts.startup)
-require(script.game_startup)
+
+local Players = game:GetService("Players")
+
+local commands_system = script.commands_system
+local commands_folder = commands_system.commands
+local scripts_folder = commands_system.scripts
+local misc_folder = commands_system.misc
+
+local Command = require(scripts_folder.command)
+local Commander = require(scripts_folder.commander)
+local settings_module = require(commands_system.settings)
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local commands_system_shared = ReplicatedStorage.Common.commands_system_shared
+local remote_event = commands_system_shared.RemoteEvent
+
+local temporary_ban = require(scripts_folder.temporary_ban)
+local trello_api = require(scripts_folder.trello_api)
+
+-- Trello Items
+local boardID = trello_api:GetBoardID("Adonis Control Center")
+local listID = trello_api:GetListID("Banlist",boardID)
+
+local function check_trello_ban(player)
+	local listInfo = trello_api:GetCardsInList(listID)
+	for _, banned_player_card in pairs(listInfo) do
+		local player_query = string.format("%s:%s", player.Name, player.UserId)
+		if banned_player_card.name == player_query then
+			return true
+		end
+	end
+end
+
+local function playerAdded(player)
+	if temporary_ban.find(player.UserId) then
+		player:Kick("Temporary Banned")
+	end
+	if check_trello_ban(player) then
+		player:Kick("Trello Banned")
+	end
+end
+
+for _, player in ipairs(Players:GetPlayers()) do
+    coroutine.wrap(playerAdded)(player)
+end
+
+Players.PlayerAdded:Connect(playerAdded)
