fca552d52efda8b316c682fadba72b08e4c692b4
drawn <69976177+itsdrawn@users.noreply.github.com>
2021-07-29 05:32:05 -0400

    Update game_notifications.lua

--- src/client/commands_system_client/game_notifications.lua
+++ src/client/commands_system_client/game_notifications.lua
@@ -5,15 +5,15 @@ end
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local commands_system_shared = ReplicatedStorage.Common.commands_system_shared
 local remote_event = commands_system_shared.RemoteEvent_notifications
-
-remote_event.OnClientEvent:Connect(function(TitleValue, TextValue, iconValue, DurationValue, Button1Value, Button2Value)
+-- TitleValue, TextValue, iconValue, DurationValue, Button1Value, Button2Value
+remote_event.OnClientEvent:Connect(function(options)
 	game.StarterGui:SetCore("SendNotification", {
-		Title = TitleValue;
-		Text = TextValue;
-		Icon = iconValue; 
-		Duration = DurationValue;
-		Button1 = Button1Value; 
-		Button2 = Button2Value;
+		Title = options.titleValue;
+		Text = options.textValue;
+		Icon = options.iconValue; 
+		Duration = options.durationValue;
+		Button1 = options.button1Value; 
+		Button2 = options.button2Value;
 	})
 end)
 
